fc10b5330c4f0e05037b57f5f151d10d8b893a17
rick9999 <50361290+rick9999@users.noreply.github.com>
2019-09-18 19:15:23 +0100

    Update garagem_final.ino

--- Garagem/garagem_final.ino
+++ Garagem/garagem_final.ino
@@ -35,12 +35,12 @@
 #define RELAY_ON 1
 #define RELAY_OFF 0
 
-Bounce debouncerA = Bounce();   //"micro switch estado do portão"
-Bounce debouncerB = Bounce();   //"botão acionamento abertura fecho portão"
+//Bounce debouncerA = Bounce();   //"botão acionamento abertura fecho portão"
+Bounce debouncerB = Bounce();   //"micro switch estado do portão"
 Bounce debouncerC = Bounce();   //"botão acionament iluminação"
 
 int oldValueA = 0;
-int oldValueB = 0;
+int oldValueB = -1;
 int oldValueC = 0;
 int oldValueD = 0;
 
@@ -50,7 +50,7 @@ bool stateC;            // Estado iluminação
 bool stateD;            // Estado Aux
 
 MyMessage msgA(CHILD_ID1, V_LIGHT);
-MyMessage msgB(CHILD_ID2, V_STATUS);
+MyMessage msgB(CHILD_ID2, V_TRIPPED);
 MyMessage msgC(CHILD_ID3, V_LIGHT);
 MyMessage msgD(CHILD_ID4, V_LIGHT);
 
@@ -62,8 +62,8 @@ void setup()
   pinMode(BUTTON3_PIN, INPUT_PULLUP);
 
   // After setting up the button, setup debouncer
-  debouncerA.attach(BUTTON1_PIN);
-  debouncerA.interval(5);
+ // debouncerA.attach(BUTTON1_PIN);
+ // debouncerA.interval(5);
   debouncerB.attach(BUTTON2_PIN);
   debouncerB.interval(5);
   debouncerC.attach(BUTTON3_PIN);
@@ -79,7 +79,7 @@ void setup()
   pinMode(RELAY3_PIN, OUTPUT);
 
   // Set relay to last known state (using eeprom storage)
-  stateA = loadState(CHILD_ID1);
+  /*stateA = loadState(CHILD_ID1);
   digitalWrite(RELAY1_PIN, stateA ? RELAY_ON : RELAY_OFF);
 
   stateB = loadState(CHILD_ID2);
@@ -89,6 +89,7 @@ void setup()
 
   stateD = loadState(CHILD_ID4);
   digitalWrite(RELAY3_PIN, stateD ? RELAY_ON : RELAY_OFF);
+  */
 }
 
 void presentation()
@@ -96,24 +97,24 @@ void presentation()
   // Send the sketch version information to the gateway and Controller
   sendSketchInfo("Controlo garagem", "1.0");
   // Register all sensors to gw (they will be created as child devices)
-  present(CHILD_ID1, S_BINARY);
+  present(CHILD_ID1, S_LIGHT);
   present(CHILD_ID2, S_DOOR);
-  present(CHILD_ID3, S_BINARY);
-  present(CHILD_ID4, S_BINARY);
+  present(CHILD_ID3, S_LIGHT);
+  present(CHILD_ID4, S_LIGHT);
 }
 
 void loop()
 {
-  debouncerA.update();                           // Estado do portão (magnetic Switch portão)
+ /* debouncerA.update();                           // botão acionamento abertura fecho portão
   int valueA = debouncerA.read();
   if (valueA != oldValueA && valueA == 0) {
     send(msgA.set(stateA ? false : true), true); // Send new state and request ack back
   }
-  oldValueA = valueA;
-  debouncerB.update();                            // botão acionamento abertura fecho portão
+  oldValueA = valueA;*/
+  debouncerB.update();                            // Estado do portão (magnetic Switch portão)
   int valueB = debouncerB.read();
-  if (valueB != oldValueB && valueB == 0) {
-    send(msgB.set(stateB ? false : true), true); // Send new state and request ack back
+  if (valueB != oldValueB ) {
+    send(msgB.set(valueB==HIGH ? 1 : 0)); // Send new state and request ack back
   }
   oldValueB = valueB;
   debouncerC.update();                            // botão acionamento iluminação
